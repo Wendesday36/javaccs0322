98266c397dc026ac334ef09ffed1dbca3b848de5
Wendesday36 <bertazoeanna@gmail.com>
2023-03-22 08:56:54 +0100

    for ->while

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
-import  {SZAMLISTA} from "./adat.js";
-console.log(SZAMLISTA)
-$(function(){
-    keveres1(SZAMLISTA)
-})
-function keveres1(lista){
-    
-        let van
-        let megKever=[]
-        let szamok
-        while (megKever.length<6) {
-        szamok=Math.floor(Math.random()*6)
-        van=false
-        for (let index = 0; index < megKever.length; index++) {
-        if (szamok==megKever[index]){
-        van=true
-        }
-        
-        }
-        if (van==false)
-        {
-        megKever.push(szamok)
-        }
-        
-        }
-        console.log(megKever)
-        return megKever
-        }
+import { SZAMLISTA } from "./adat.js";
+console.log(SZAMLISTA);
+$(function () {
+  keveres1(SZAMLISTA);
+});
+function keveres1(lista) {
+  let van;
+  let megKeverIndex = [];
+  let szamok;
+  while (megKeverIndex.length < 6) {
+    szamok = Math.floor(Math.random() * 6);
+    van = false;
+    let index = 0;
+    while (index < megKeverIndex.length && !(szamok == megKeverIndex[index])) {
+      index++;
+    }
+    if (van == false) {
+      megKeverIndex.push(szamok);
+    }
+  }
+  console.log(megKeverIndex);
+  return megKeverIndex;
+}
