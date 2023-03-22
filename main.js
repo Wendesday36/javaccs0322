b947f616a6133ad984d3138a5f9ed71abec9e31e
Wendesday36 <bertazoeanna@gmail.com>
2023-03-22 08:51:08 +0100

    megKever

--- main.js
+++ main.js
@@ -5,4 +5,24 @@ $(function(){
 })
 function keveres1(lista){
     
-}
+        let van
+        let megKever=[]
+        let szamok
+        while (megKever.length<6) {
+        szamok=Math.floor(Math.random()*6)
+        van=false
+        for (let index = 0; index < megKever.length; index++) {
+        if (szamok==megKever[index]){
+        van=true
+        }
+        
+        }
+        if (van==false)
+        {
+        megKever.push(szamok)
+        }
+        
+        }
+        console.log(megKever)
+        return megKever
+        }
