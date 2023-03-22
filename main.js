d8b360f680071a37cb3b6697ec65d316935ab950
Wendesday36 <bertazoeanna@gmail.com>
2023-03-22 09:01:15 +0100

    megkever

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ function keveres1(lista) {
   let van;
   let megKeverIndex = [];
   let szamok;
-  while (megKeverIndex.length < 6) {
-    szamok = Math.floor(Math.random() * 6);
+  while (megKeverIndex.length < lista.length) {
+    szamok = Math.floor(Math.random() * lista.length);
     van = false;
     let index = 0;
     while (index < megKeverIndex.length && !(szamok == megKeverIndex[index])) {
       index++;
     }
-    if (van == false) {
+    if (index >= megKeverIndex.length) {
       megKeverIndex.push(szamok);
     }
   }
